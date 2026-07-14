9429c3149536f576029b6ef18fffb5e10d564d2d
PabloX110 <PabloX110@users.noreply.github.com>
2026-07-13 22:04:54 -0600

    Crear receta: marca ingredientes que tienes y genera recetas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'midia-v11';
+const CACHE = 'midia-v12';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
